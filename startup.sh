38e7f21f3af2a2c6d5d91c17eb22f51aa9f8752e
Jerome Pinguet <jerome@jerome.cc>
2016-11-29 16:55:00 +0300

    in progress

--- startup.sh
+++ startup.sh
@@ -4,12 +4,13 @@ mkdir -p /var/run/sshd
 
 # create an ubuntu user
 # PASS=`pwgen -c -n -1 10`
-PASS=ubuntu
-# echo "Username: ubuntu Password: $PASS"
-id -u ubuntu &>/dev/null || useradd --create-home --shell /bin/bash --user-group --groups adm,sudo ubuntu
+PASS=freehealth
+USER=freehealth
+echo "Username: $USER Password: $PASS"
+id -u $USER &>/dev/null || useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
 echo "ubuntu:$PASS" | chpasswd
-sudo -u ubuntu -i bash -c "mkdir -p /home/ubuntu/.config/pcmanfm/LXDE/ \
-    && cp /usr/share/doro-lxde-wallpapers/desktop-items-0.conf /home/ubuntu/.config/pcmanfm/LXDE/"
+# sudo -u ubuntu -i bash -c "mkdir -p /home/ubuntu/.config/pcmanfm/LXDE/ \
+#    && cp /usr/share/doro-lxde-wallpapers/desktop-items-0.conf /home/ubuntu/.config/pcmanfm/LXDE/"
 
 cd /web && ./run.py > /var/log/web.log 2>&1 &
 nginx -c /etc/nginx/nginx.conf
